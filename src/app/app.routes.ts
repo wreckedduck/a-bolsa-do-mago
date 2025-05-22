2ff04948ce629dc2d679716b30a9f3a876b13168
Levy Alves Mr. Duck <levyhanunes@gmail.com>
2025-05-22 20:41:37 -0300

    feat: add About and Home components with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router'
 
-export const routes: Routes = []
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./pages/about/about.component').then((m) => m.AboutComponent),
+  },
+]
